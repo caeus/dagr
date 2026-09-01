b96822190501cb3fb82ca060dec7eadcfeb2bbf5
caeus <camilo.a.navas@gmail.com>
2026-09-01 15:55:21 +0200

    Upgrade the pinned TypeScript stack

--- stacks/ts/dagr.index.js
+++ stacks/ts/dagr.index.js
@@ -1,4 +1,5 @@
-const STACKS_COMMIT = '921919d6ec43affc2f574620292c405688e79e53'
+const STACKS_COMMIT = 'a2dc4a147afe56ca006e49f405e55cd55bdf5d15'
+const DI_COMMIT = 'a8782212bc94065dff632749f884ff84022d314e'
 
 export default {
   '/': {
@@ -11,11 +12,12 @@ export default {
           'cd /src',
           'git remote add origin https://github.com/caeus/dagr-stacks.git',
           'git sparse-checkout init --cone',
-          'git sparse-checkout set typescript di',
+          'git sparse-checkout set typescript',
           `git fetch --depth=1 --filter=blob:none origin ${STACKS_COMMIT}`,
           'git checkout --detach FETCH_HEAD',
+          `git fetch --depth=1 --filter=blob:none origin ${DI_COMMIT}`,
           'rm -f /src/typescript/di/dagr.index.js',
-          'cp /src/di/dagr.di.js /src/typescript/di/dagr.di.js',
+          `git show ${DI_COMMIT}:di/dagr.di.js > /src/typescript/di/dagr.di.js`,
           `find /src/typescript -type f -name '*.js' -exec sed -i 's#//di//#//di/#g' {} \\;`,
         ].join(' && '),
       },
