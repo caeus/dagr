90753c6f3a3af75d7cb4381d09627ce60bdf03b2
caeus <camilo.a.navas@gmail.com>
2026-09-01 15:48:04 +0200

    Mount the TypeScript dagr stack

---
+++ stacks/ts/dagr.index.js
@@ -0,0 +1,23 @@
+const STACKS_COMMIT = '921919d6ec43affc2f574620292c405688e79e53'
+
+export default {
+  '/': {
+    FROM: 'alpine:3.22',
+    steps: [
+      { RUN: 'apk add --no-cache git' },
+      {
+        RUN: [
+          'git init /src',
+          'cd /src',
+          'git remote add origin https://github.com/caeus/dagr-stacks.git',
+          'git sparse-checkout init --cone',
+          'git sparse-checkout set typescript',
+          `git fetch --depth=1 --filter=blob:none origin ${STACKS_COMMIT}`,
+          'git checkout --detach FETCH_HEAD',
+        ].join(' && '),
+      },
+      { WORKDIR: '/src/typescript' },
+    ],
+    IGNORE: [],
+  },
+}
