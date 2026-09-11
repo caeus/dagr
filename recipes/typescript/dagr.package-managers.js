f9d078c63aac0922b3f8d2b42cb9f8f141073935
caeus <camilo.a.navas@gmail.com>
2026-09-11 09:43:29 +0200

    Use named dependencies in package manager contributions

--- recipes/typescript/dagr.package-managers.js
+++ recipes/typescript/dagr.package-managers.js
@@ -14,17 +14,31 @@ export const fileTarballs = (manifest, localPackages, _context) => localPackages
 const packDestination = command => slug =>
   `mkdir -p /tmp/pack /out && ${command} --pack-destination /tmp/pack && mv /tmp/pack/*.tgz /out/${slug}.tgz`
 
+const packCommand = command => ({
+  '/package-manager/install-manifest': rdk.value(fileTarballs),
+  '/package-manager/exec': rdk.value(invocation => `${command.exec}${invocation}`),
+  '/package-manager/script': rdk.value(invocation => invocation),
+  '/package-manager/install': rdk.value(command.install),
+  '/package-manager/pack': rdk.value(command.pack),
+  '/command/pack/package': commandContribution(command.pack),
+})
+
+const commandContribution = _unused => undefined
+
 export const npm = () => rdk.graph({
   '/package-manager/install-manifest': rdk.value(fileTarballs),
   '/package-manager/exec': rdk.value(invocation => `npm exec -- ${invocation}`),
   '/package-manager/script': rdk.value(invocation => invocation),
   '/package-manager/install': rdk.value(host => `npm install --include=dev${host ? ` --os=${host.os} --cpu=${host.arch}` : ''}`),
   '/package-manager/pack': rdk.value(packDestination('npm pack')),
-  '/command/pack/package': command(['/package-manager/pack', '/package/slug'], {
+  '/command/pack/package': command({
+    pack: rdk.one('/package-manager/pack'),
+    slug: rdk.one('/package/slug'),
+  }, {
     for: ['pack', 'publish'],
-    run: (pack, slug) => ({ shell: pack(slug) }),
+    run: ({ pack, slug }) => ({ shell: pack(slug) }),
   }),
-  '/file/package-manager': file([], {
+  '/file/package-manager': file({}, {
     for: DEVELOPMENT_INTENTS,
     render: () => [],
   }),
@@ -36,13 +50,18 @@ export const pnpm = () => rdk.graph({
   '/package-manager/script': rdk.value(invocation => invocation),
   '/package-manager/install': rdk.value(host => `pnpm install --prod=false${host ? ` --os ${host.os} --cpu ${host.arch}` : ''}`),
   '/package-manager/pack': rdk.value(packDestination('pnpm pack')),
-  '/command/pack/package': command(['/package-manager/pack', '/package/slug'], {
+  '/command/pack/package': command({
+    pack: rdk.one('/package-manager/pack'),
+    slug: rdk.one('/package/slug'),
+  }, {
     for: ['pack', 'publish'],
-    run: (pack, slug) => ({ shell: pack(slug) }),
+    run: ({ pack, slug }) => ({ shell: pack(slug) }),
   }),
-  '/file/package-manager': file([['/requirement/build-scripts/**']], {
+  '/file/package-manager': file({
+    builds: rdk.many('/requirement/build-scripts/**'),
+  }, {
     for: DEVELOPMENT_INTENTS,
-    render(context, builds) {
+    render(context, { builds }) {
       const allowBuilds = factsFor(builds, context.intent)
       return allowBuilds.length === 0
         ? []
@@ -75,11 +94,14 @@ export const yarn = () => rdk.graph({
   '/package-manager/script': rdk.value(invocation => invocation),
   '/package-manager/install': rdk.value(() => 'yarn install --no-immutable'),
   '/package-manager/pack': rdk.value(slug => `mkdir -p /out && yarn pack --out /out/${slug}.tgz`),
-  '/command/pack/package': command(['/package-manager/pack', '/package/slug'], {
+  '/command/pack/package': command({
+    pack: rdk.one('/package-manager/pack'),
+    slug: rdk.one('/package/slug'),
+  }, {
     for: ['pack', 'publish'],
-    run: (pack, slug) => ({ shell: pack(slug) }),
+    run: ({ pack, slug }) => ({ shell: pack(slug) }),
   }),
-  '/file/package-manager': file([], {
+  '/file/package-manager': file({}, {
     for: DEVELOPMENT_INTENTS,
     render: context => writeYaml('/repo/.yarnrc.yml', {
       enableScripts: false,
